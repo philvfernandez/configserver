63c6ff604527ff5250e065041d493d60f6b9c816
Phil Fernandez <philvfernandez@gmail.com>
2023-03-01 16:07:33 -0700

    Section 43 - Built config server service and configuration files to load from classpath.

--- src/main/java/com/philf/ms/configserver/ConfigserverApplication.java
+++ src/main/java/com/philf/ms/configserver/ConfigserverApplication.java
@@ -2,8 +2,10 @@ package com.philf.ms.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer  //Makes this application a configuration server that can read the configurations.
 public class ConfigserverApplication {
 
     public static void main(String[] args) {
